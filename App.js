d16b36da3fd77dcb38975511f66e8380848b1824
Casper Smits <casper@caspersmits.nl>
2019-03-28 17:03:09 +0100

    Add essential packages

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello, World!</Text>
+        <Image source={pic} style={{width: 293, height: 110}} />
       </View>
     );
   }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
+});
+
+export default createAppContainer(AppNavigator);
+
 
 const styles = StyleSheet.create({
   container: {
