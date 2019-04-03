7e0219adb6a8e730781b10e0a30eaf856a400144
Casper Smits <casper@caspersmits.nl>
2019-04-03 10:36:14 +0200

    Update app.json

--- Tasks.js
+++ Tasks.js
@@ -1,25 +1,71 @@
-import { TaskManager } from 'expo';
+import { TaskManager, Permissions, Notifications } from 'expo';
 
 export const BACKGROUND_TASK_LOCATION_TRACKING = 'backgroundTask/locationTracking';
+export const PUSH_ENDPOINT = 'http://webhook.site/7a50c22b-2b3e-4912-8d5b-33a024ce21b0';
 
-export const defineLocationBackgroundTask = () => {
-  TaskManager.defineTask(BACKGROUND_TASK_LOCATION_TRACKING, async ({ data: { locations }, error }) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log('locations via background task', [
-        ...locations.map(v => v.coords)
-      ]);
-      const location = locations[0];
-      const { latitude, longitude } = location.coords;
+export const defineLocationBackgroundTask = async () => {
+  TaskManager.defineTask(BACKGROUND_TASK_LOCATION_TRACKING,
+    async ({ data: { locations }, error }) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('locations via background task', [
+          ...locations.map(v => v.coords)
+        ]);
+        const location = locations[0];
+        const { latitude, longitude } = location.coords;
 
-      fetch('http://webhook.site/7a50c22b-2b3e-4912-8d5b-33a024ce21b0', {
-        method: 'POST',
-        body: JSON.stringify({
-          latitude,
-          longitude
-        })
-      });
-    }
+        fetch('http://webhook.site/7a50c22b-2b3e-4912-8d5b-33a024ce21b0', {
+          method: 'POST',
+          body: JSON.stringify({
+            latitude,
+            longitude
+          })
+        });
+      }
+    });
+};
+
+export const registerForPushNotificationsAsync = async () => {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== 'granted') {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== 'granted') {
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  const token = await Notifications.getExpoPushTokenAsync();
+
+  // POST the token to your backend server from where you can retrieve
+  // it to send push notifications.
+  const response = await fetch(PUSH_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token: {
+        value: token,
+      },
+      user: {
+        username: 'Brent',
+      },
+    }),
   });
+
+  console.log(response.json());
 };
